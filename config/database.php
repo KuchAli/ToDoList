357e2c491848ff316507949e0c563197879be7bc
Mister K <khidirgaminghd@gmail.com>
2026-04-24 22:47:58 +0700

    feat : nambahin db dan konekin

--- config/database.php
+++ config/database.php
@@ -1,3 +1,8 @@
 <?php
+$conn = mysqli_connect("localhost","root","","todolist_db");
 
-    
+if($conn ->connect_error) {
+    die ("Connection failed : ".$conn->connect_error);
+}
+echo "Connected successfully";
+?>
